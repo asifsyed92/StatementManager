081c16e7508c1c7b7f91abafeedb1e686dbbd6b8
Asif Syed <get.asifuddinsyed@gmail.com>
2023-05-23 19:57:06 -0400

    Fixing imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import org.apache.pdfbox.text.PDFTextStripper;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
