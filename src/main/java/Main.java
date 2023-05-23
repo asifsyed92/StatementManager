4c09abe626900fde98c14c98ffc1b6d0d4ae28fc
Asif Syed <get.asifuddinsyed@gmail.com>
2023-05-23 19:56:31 -0400

    A sample pdf reader code added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String expectedText;
+        File file = new File("src/main/resources/sample.pdf");
+        try {
+            PDDocument document = PDDocument.load(file);
+            PDFTextStripper stripper = new PDFTextStripper();
+            expectedText = stripper.getText(document);
+            document.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(expectedText);
     }
 }
